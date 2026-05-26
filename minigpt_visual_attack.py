fc05cc9a7895f2e79ce00c5cffe54af5aa46d5c0
Shreyansh Modi <shrynshmodi@gmail.com>
2026-05-26 11:43:39 +0000

    added

--- minigpt_visual_attack.py
+++ minigpt_visual_attack.py
@@ -114,13 +114,13 @@ if not args.constrained:
 
 
     adv_img_prompt = my_attacker.attack_unconstrained(text_prompt_template,
-                                                            img=img, batch_size=4,
-                                                            num_iter=500, alpha=args.alpha/255)
+                                                            img=img, batch_size=45,
+                                                            num_iter=5000, alpha=args.alpha/255)
 
 else:
     adv_img_prompt = my_attacker.attack_constrained(text_prompt_template,
-                                                            img=img, batch_size= 4,
-                                                            num_iter=500, alpha=args.alpha / 255,
+                                                            img=img, batch_size= 45,
+                                                            num_iter=5000, alpha=args.alpha / 255,
                                                             epsilon=args.eps / 255)
 
 save_image(adv_img_prompt, '%s/bad_prompt.bmp' % args.save_dir)
